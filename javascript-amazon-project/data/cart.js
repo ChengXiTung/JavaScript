3ed552c5d268113d7a1d3ef1b0328c6ebb1fa0de
LemonPie <vincenttung1379@gmail.com>
2025-06-08 02:23:39 +0800

    add cart persistence and remove item functionality in checkout process

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,10 +1,14 @@
-export const cart = [{
+export let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
 }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
-}];
+}]; // in English: if there is a cart in localStorage, use it; otherwise, start with an default cart
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId) {
   let matchingItem;
@@ -23,4 +27,22 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
-};
+
+    saveToStorage();
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach(cartItem => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+    cart.length = 0; // Clear the original cart
+    newCart.forEach(cartItem => {
+        cart.push(cartItem);
+    });
+    console.log(cart);
+
+    saveToStorage();
+}
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,8 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
+hello();
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -28,7 +29,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                $${formatCurrency(matchingProduct.priceCents)}
+                ${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
                 <span>
@@ -37,7 +38,8 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link"
+                data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -91,5 +93,10 @@ cart.forEach((cartItem) => {
         </div>
     `
 });
-console.log(cartSummaryHTML);
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+    });
+});
